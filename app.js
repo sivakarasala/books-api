7f1e65ab75ab2d168cb6aebc7f46065f36e2b891
Siva Krishna <shivataraya@gmail.com>
2019-12-15 15:45:10 -0600

    setup router to get books  req

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 
 const app = express();
-
+const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+
+bookRouter.route("/books").get((req, res) => {
+  const response = { shiva: "From Kailash" };
+
+  res.json(response);
+});
+
+app.use("/api", bookRouter);
 
 app.get("/", (req, res) => {
   res.send("Aum Namah Shivaya");
