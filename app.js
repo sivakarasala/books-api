b2e3fb7c48af89bc352c48f16fdd3235834047fb
Siva Krishna <shivataraya@gmail.com>
2019-12-15 16:36:50 -0600

    add post query

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 const db = mongoose.connect("mongodb://localhost/bookAPI", {
@@ -10,18 +11,29 @@ const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require("./models/bookModel");
 
-bookRouter.route("/books").get(async (req, res) => {
-  try {
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+bookRouter
+  .route("/books")
+  .post(async (req, res) => {
+    const book = new Book(req.body);
+
+    await book.save();
+    return res.status(201).json(book);
+  })
+  .get(async (req, res) => {
+    try {
+      const query = {};
+      if (req.query.genre) {
+        query.genre = req.query.genre;
+      }
+      const books = await Book.find(query);
+      res.json(books);
+    } catch (err) {
+      res.send(err);
     }
-    const books = await Book.find(query);
-    res.json(books);
-  } catch (err) {
-    res.send(err);
-  }
-});
+  });
 
 bookRouter.route("/books/:bookId").get(async (req, res) => {
   try {
