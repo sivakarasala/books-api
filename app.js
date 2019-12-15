bcd4288cbb383e0159c0e59ed6665093331d93da
Siva Krishna <shivataraya@gmail.com>
2019-12-15 16:44:10 -0600

    code refactor

--- app.js
+++ app.js
@@ -7,42 +7,13 @@ const db = mongoose.connect("mongodb://localhost/bookAPI", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-const bookRouter = express.Router();
+
 const port = process.env.PORT || 3000;
 const Book = require("./models/bookModel");
+const bookRouter = require("./routes/bookRouter")(Book);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-bookRouter
-  .route("/books")
-  .post(async (req, res) => {
-    const book = new Book(req.body);
-
-    await book.save();
-    return res.status(201).json(book);
-  })
-  .get(async (req, res) => {
-    try {
-      const query = {};
-      if (req.query.genre) {
-        query.genre = req.query.genre;
-      }
-      const books = await Book.find(query);
-      res.json(books);
-    } catch (err) {
-      res.send(err);
-    }
-  });
-
-bookRouter.route("/books/:bookId").get(async (req, res) => {
-  try {
-    const book = await Book.findById(req.params.bookId);
-    res.json(book);
-  } catch (err) {
-    res.send(err);
-  }
-});
 
 app.use("/api", bookRouter);
 
---
+++ routes/bookRouter.js
@@ -0,0 +1,38 @@
+const express = require("express");
+
+function routes(Book) {
+  const bookRouter = express.Router();
+  bookRouter
+    .route("/books")
+    .post(async (req, res) => {
+      const book = new Book(req.body);
+
+      await book.save();
+      return res.status(201).json(book);
+    })
+    .get(async (req, res) => {
+      try {
+        const query = {};
+        if (req.query.genre) {
+          query.genre = req.query.genre;
+        }
+        const books = await Book.find(query);
+        res.json(books);
+      } catch (err) {
+        res.send(err);
+      }
+    });
+
+  bookRouter.route("/books/:bookId").get(async (req, res) => {
+    try {
+      const book = await Book.findById(req.params.bookId);
+      res.json(book);
+    } catch (err) {
+      res.send(err);
+    }
+  });
+
+  return bookRouter;
+}
+
+module.exports = routes;
