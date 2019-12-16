cf0f27bc2308e6cc20536ef7fd85188620b17087
Siva Krishna <shivataraya@gmail.com>
2019-12-15 18:14:33 -0600

    integration test with supertest

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const app = express();
-const db = mongoose.connect("mongodb://localhost/bookAPI", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+if (process.env.ENV === "Test") {
+  console.log("Test");
+  const db = mongoose.connect("mongodb://localhost/bookAPI_Test", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+} else {
+  console.log("Prod");
+  const db = mongoose.connect("mongodb://localhost/bookAPI", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+}
 
 const port = process.env.PORT || 3000;
 const Book = require("./models/bookModel");
@@ -21,6 +30,8 @@ app.get("/", (req, res) => {
   res.send("Aum Namah Shivaya");
 });
 
-app.listen(port, () => {
+app.server = app.listen(port, () => {
   console.log(`Hara Hara Mahadeva ${port}`);
 });
+
+module.exports = app;
